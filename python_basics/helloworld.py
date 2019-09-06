debf1426b12aa8c84e0b8814bf2fd37415193ef8
Shivabindraban <shiva.bindraban@gmail.com>
2019-09-06 15:30:52 +0200

    Modified the python-code with questions

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,2 +1,9 @@
-s='Hello World'
-print(s)
+s='Hi';
+print(s);
+username = input("What is your name? ");
+print("Hello", username);
+YesorNo = input("Are you older than 18? Fill in Yes or No: ");
+if YesorNo == "Yes":
+    print("Welcome to the site of many wonders", username)
+else:
+    print("Oh Oh", username, "since you are too young, you may not visit this site!");
