30fed8266336cb87c70c71b01815a9a1da4e77bd
Shivabindraban <shiva.bindraban@gmail.com>
2019-09-06 18:11:30 +0200

    Added the possibility for using lowercase answers

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,9 +1,8 @@
-s='Hi';
-print(s);
-username = input("What is your name? ");
-print("Hello", username);
-YesorNo = input("Are you older than 18? Fill in Yes or No: ");
-if YesorNo == "Yes":
+print("Hi")
+username = input("What is your name? ")
+print("Hello", username)
+YesorNo = input("Are you older than 18? Fill in Yes or No: ")
+if YesorNo == "Yes" or "yes" or "ja":
     print("Welcome to the site of many wonders", username)
 else:
     print("Oh Oh", username, "since you are too young, you may not visit this site!");
