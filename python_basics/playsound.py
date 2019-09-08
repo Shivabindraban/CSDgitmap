3c8521cad5c6572c4848d8040af7c4593f25c17d
Shivabindraban <shiva.bindraban@gmail.com>
2019-09-08 14:39:52 +0200

    Added a for loop for multiple plays

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,1 +1,10 @@
-import simpleaudio.functionchecks as fc
+# import simpleaudio as sa
+#
+# wave_obj = sa.WaveObject.from_wave_file("/mnt/d/Samples/Camou's Selection/Kick/kick.wav")
+
+PlayAmount = input('How many times would you like to play your sample? ')
+for PlayAmount in range(int(PlayAmount)):
+    # play_obj = wave_obj.play()
+    # play_obj.wait_done()
+    print(PlayAmount)
+    print('dong')
