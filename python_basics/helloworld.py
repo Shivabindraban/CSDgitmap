22ec49f55386423d2098f95a6dba3e945ad8bb93
Shivabindraban <shiva.bindraban@gmail.com>
2019-09-08 14:03:52 +0200

    Fixed the or part in the If statement

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -2,7 +2,7 @@ print("Hi")
 username = input("What is your name? ")
 print("Hello", username)
 YesorNo = input("Are you older than 18? Fill in Yes or No: ")
-if YesorNo == "Yes" or "yes" or "ja":
+if YesorNo == "Yes"or YesorNo == "yes":
     print("Welcome to the site of many wonders", username)
 else:
     print("Oh Oh", username, "since you are too young, you may not visit this site!");
