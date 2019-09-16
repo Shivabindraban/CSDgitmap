04109c5ac75415a0ebbf2d20c55c617bbe1d9840
Shivabindraban <shiva.bindraban@gmail.com>
2019-09-17 00:11:22 +0200

    Practice codes with lists and for loops

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -3,8 +3,11 @@
 # wave_obj = sa.WaveObject.from_wave_file("/mnt/d/Samples/Camou's Selection/Kick/kick.wav")
 
 PlayAmount = input('How many times would you like to play your sample? ')
-for PlayAmount in range(int(PlayAmount)):
-    # play_obj = wave_obj.play()
-    # play_obj.wait_done()
-    print(PlayAmount)
-    print('dong')
+if( PlayAmount.isdigit()):
+    for PlayAmount in range(int(PlayAmount)):
+        # play_obj = wave_obj.play()
+        # play_obj.wait_done()
+        print(PlayAmount)
+        print('dong')
+else:
+    print("You did not enter a number")
