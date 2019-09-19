0f543477010699791bd5df28969675c993208058
Shiva Bindraban <shiva.bindraban@gmail.com>
2019-09-19 14:57:00 +0200

    Enabled simpleaudio module to play samples

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,12 +1,12 @@
-# import simpleaudio as sa
-#
-# wave_obj = sa.WaveObject.from_wave_file("/mnt/d/Samples/Camou's Selection/Kick/kick.wav")
+import simpleaudio as sa
+
+wave_obj = sa.WaveObject.from_wave_file("/media/shiva/Data/Samples/Linuxmap/kick.wav")
 
 PlayAmount = input('How many times would you like to play your sample? ')
 if( PlayAmount.isdigit()):
     for PlayAmount in range(int(PlayAmount)):
-        # play_obj = wave_obj.play()
-        # play_obj.wait_done()
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
         print(PlayAmount)
         print('dong')
 else:
